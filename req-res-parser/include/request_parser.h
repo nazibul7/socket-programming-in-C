0b300c01b56fa3fd2f7f5c8bef235aaf9f831998
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-07-20 20:29:19 +0530

    docs: add inline comment for parse_http_request in request_parser.h

--- req-res-parser/include/request_parser.h
+++ req-res-parser/include/request_parser.h
@@ -1,4 +1,13 @@
 #pragma once
-#include "http_types.h"
+#include "http_types.h" // For HTTP type struct
+
+/**
+ * @file request_parser.h
+ * @brief Parses a raw HTTP request from a buffer into an HttpRequest structure.
+ *
+ * @param buffer Raw HTTP request
+ * @param req Pointer to a HttpRequest struct where the parsed request fields will be stored or to be populated.
+ * @return 0 on success, -1 on failure
+ */
 
 int parse_http_request(char *buffer, HttpRequest *req);
